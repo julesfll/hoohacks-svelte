bd2f57647bed6911f1b4ebaa0e5d33f2cdaf2487
Jules Le Menestrel <juleslemenestrel@gmail.com>
2022-03-26 16:17:43 -0400

    Add finished code

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Item from "./Item.svelte";
+  
+    let todos = [
+      { done: false, text: 'Make a to-do list'}
+    ];
+    let value;
+  
+    function handleSubmit() {
+      todos = [...todos, { done: false, text: value }];
+    }
+  
+    $: filtered = todos.filter(todo => !todo.done);
+  </script>
+  
+  <h1>To-do List</h1>
+  <p>{filtered.length} items left</p>
+  
+  {#each filtered as todo}
+    <Item bind:todo />
+  {:else}
+    <p>Add a to-do!</p>
+  {/each}
+  
+  <form on:submit|preventDefault={handleSubmit}>
+    <input bind:value/>
+  </form>
+  <button on:click={handleSubmit}>Add to-do</button>
---
+++ src/Item.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let todo;
+  import { fly } from 'svelte/transition';
+</script>
+
+<li transition:fly={{y: 50, duration: 500}}>
+  <input type="checkbox" bind:checked={todo.done}>
+  {todo.text}
+</li>
+
+<style>
+  li {
+    list-style-type: none;
+  }
+</style>
